9f0a7b64e0f693b8aea09604b2a083991d61fd45
harrugg <harrugg@localhost.localdomain>
2019-03-15 14:34:36 +0300

    push

--- controller/admin/reservation.php
+++ controller/admin/reservation.php
@@ -212,7 +212,7 @@ include('db.php')
 							else
 							{
 							
-									$con=mysqli_connect("localhost","root","","hotel");
+									$con=mysqli_connect("localhost","root","harr","hotel");
 									$check="SELECT * FROM roombook WHERE email = '$_POST[email]'";
 									$rs = mysqli_query($con,$check);
 									$data = mysqli_fetch_array($rs, MYSQLI_NUM);
